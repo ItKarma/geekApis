90281d97ee5c9005710e14c1c8d93c55cc6c0a33
danzok <loppesofc@gmail.com>
2022-11-18 21:31:27 -0300

    refactory functions add async/await

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,40 +1,40 @@
+import { statusCode } from '../constants/statusConstats.js';
 import { ytSearch, ytDownloadMp3, ytDownloadMp4 } from '../functions/yt.js';
 
 function index(req,res){
   res.json({ message : "tudo ok"});
 }
 
-function ytsearch(req,res){
+async function ytsearch (req,res) {
+  let name = req.query.q
   try {
-    let name = req.query.q
-    ytSearch(name).then((data)=>{
-      res.status(200).send(data);
-    })
+    let data = await ytSearch(name)
+    res.status(200).send(data);
+
   } catch (error) {
-    res.status(300).json({ message : "finding nothing"})
+    res.status(500).json({ message : `${statusCode.status500}` })
   }
 }
 
-function ytDownMp3(req, res) {
+async function ytDownMp3(req, res) {
+  let name = req.query.q;
   try {
-    let name = req.query.q;
-    ytDownloadMp3(name).then((data) => {
-      res.status(200).send(data);
-    })
+    let data = await ytDownloadMp3(name);
+    res.status(200).send(data);
+
   } catch (error) {
-     res.status(300).json({ message : "try again or look of a support !"})
+     res.status(500).json({ message : `${statusCode.status500}`})
   }
 }
 
-function ytDownMp4(req, res) {
+async function ytDownMp4(req, res) {
+  let name = req.query.q;
   try {
-    let name = req.query.q;
-    ytDownloadMp4(name)
-      .then((data)=>{
-        res.status(200).send(data);
-    })
+    let data = await ytDownloadMp4(name);
+    res.status(200).send(data);
+
   } catch (error) {
-    res.status(300).json({ message : "try again or look of a support !"})
+    res.status(500).json({ message : `${statusCode.status500}` })
   }
 }
 
